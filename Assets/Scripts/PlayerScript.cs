1102db9873e2224bacf96d91a3fed218fc641410
Digitalness <60412515+Digitalness@users.noreply.github.com>
2020-12-03 18:44:28 -0500

    Uploaded unity project
    unity proj.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+  private Transform player;
+  public float speed;
+  public float maxBound, minBound;
+
+    void Start() {
+        player = GetComponent<Transform> ();
+    }
+
+
+    void Update() {
+        float h = Input.GetAxis ("Horizontal");
+
+        if (player.position.x < minBound && h < 0)
+            h = 0;
+        else if (player.position.x > maxBound && h > 0)
+            h = 0;
+
+            player.position += Vector3.right * h * speed; 
+    }
+}
